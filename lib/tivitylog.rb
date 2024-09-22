c04a1fcedb8f7bc3e7857b95baca021a1e06a521
Philip Becker <boofungal316@yahoo.com>
2024-09-21 22:15:40 -0400

    support deleting a file

--- lib/tivitylog.rb
+++ lib/tivitylog.rb
@@ -1,6 +1,7 @@
 require 'tivitylog/logger'
 require 'tivitylog/create_file'
 require 'tivitylog/modify_file'
+require 'tivitylog/delete_file'
 
 module Tivitylog
 end
---
+++ lib/tivitylog/delete_file.rb
@@ -0,0 +1,20 @@
+require 'etc'
+
+module Tivitylog
+  class DeleteFile
+    attr_reader :logger, :filename
+    def initialize(filename, logger = Logger.new)
+      @logger = logger
+      @filename = filename
+    end
+
+    def perform
+      start = Time.now
+      File.delete(filename) if File.exist?(filename)
+      logger.entry({
+        path: filename, # full path
+        activity: 'delete'
+      })
+    end
+  end
+end
